8b2628e1e98e97830a9946ab941ed40fad11a872
FacundoV21 <6756@holbertonstudents.com>
2023-12-20 08:21:42 -0800

    added task 0 and README.md

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,15 +11,12 @@ if __name__ == '__main__':
     """
 
     url = 'https://jsonplaceholder.typicode.com/'
-    #sets the api url
 
     id = argv[1]
     info = requests.get(url + f'users/{id}').json()
     tasks = requests.get(url + f'users/{id}/todos').json()
-    #requests info from the api
 
     done_tasks = [task for task in tasks if task["completed"]]
-    #creates a dict with the completed tasks
 
     print(f'Employee {info["name"]} is done with tasks', end=' ')
     print(f'({len(done_tasks)}/{len(tasks)}):')
