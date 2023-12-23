03278b0bf9f4d48c386adf1821e7e0b4e83e13ea
FacundoV21 <6756@holbertonstudents.com>
2023-12-23 13:49:00 -0800

    added task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    Exports data in JSON
+"""
+import json
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+
+    users = users.json()
+
+    data = {}
+    for user in users:
+        list = []
+        todos = requests.get(f"https://jsonplaceholder.typicode.com/\
+            todos?userId={user['id']}")
+        todos = todos.json()
+        for todo in todos:
+            dictionary = {}
+
+            dictionary["username"] = user["username"]
+            dictionary["task"] = todo["title"]
+            dictionary["completed"] = todo["completed"]
+            
+            list.append(dictionary)
+        data[f"{user['id']}"] = list
+
+    with open("todo_all_employees.json", "w", encoding="utf-8") as f:
+        f.write(json.dumps(data))
